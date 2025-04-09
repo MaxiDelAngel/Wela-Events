80bb16073b225ba1f3347c4688609d756b65b6ff
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-04-09 14:39:49 -0600

    refactor: divided header and footer into separate files and included within home view

---
+++ views/footer.php
@@ -0,0 +1,18 @@
+<style>
+    @import url("/build/styles/footer.css");
+</style>
+
+<footer class="footer">
+    <hr class="footer__divider">
+    <ul class="footer__social-media">
+        <li class="footer__social-media-icon">
+            <a href="https://www.facebook.com/kanatwela/" target="_blank"><i class='bx bxl-facebook bx-tada-hover'></i></a>
+        </li>
+        <li class="footer__social-media-icon">
+            <a href="https://www.instagram.com/kanatwela/" target="_blank"><i class='bx bx bxl-instagram bx-tada-hover'></i></a>
+        </li>
+        <li class="footer__social-media-icon">
+            <a href="https://www.linkedin.com/company/kanatwela/" target="_blank"><i class='bx bx bxl-linkedin bx-tada-hover'></i></a>
+        </li>
+    </ul>
+</footer>
--- views/home.php
+++ views/home.php
@@ -13,17 +13,7 @@
     <title>Wela-Events</title>
 </head>
 <body>
-    <header class="header">
-       <img class="header__logo" src="/build/assets/Logo Wela - completo.png" alt="Logo Wela">
-       <nav class="header__nav">
-            <ul class="header__nav-list">
-                <li class="header__nav-item"><a class="header__nav-link" href="/home">Inicio</a></li>
-                <li class="header__nav-item"><a class="header__nav-link" href="/about-us">Sobre nosotros</a></li>
-                <li class="header__nav-item"><a class="header__nav-link" href="/events">Eventos</a></li>
-                <li class="header__nav-item"><a class="header__nav-link" href="/profile">Mi perfil</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php include_once "header.php"; ?>
     <main class="main">
         <?php
             if ($_REQUEST["view"] == "" || $_REQUEST["view"] == "home") 
@@ -32,19 +22,6 @@
                 include_once $_REQUEST["view"] . ".php";
         ?>
     </main>
-    <footer class="footer">
-        <hr class="footer__divider">
-        <ul class="footer__social-media">
-            <li class="footer__social-media-icon">
-                <a href="https://www.facebook.com/kanatwela/" target="_blank"><i class='bx bxl-facebook bx-tada-hover'></i></a>
-            </li>
-            <li class="footer__social-media-icon">
-                <a href="https://www.instagram.com/kanatwela/" target="_blank"><i class='bx bx bxl-instagram bx-tada-hover'></i></a>
-            </li>
-            <li class="footer__social-media-icon">
-                <a href="https://www.linkedin.com/company/kanatwela/" target="_blank"><i class='bx bx bxl-linkedin bx-tada-hover'></i></a>
-            </li>
-        </ul>
-    </footer>
+    <?php include_once "footer.php"; ?>
 </body>
 </html>
